6e8af57bd3848b352b44f5d85e6e91c67bcec334
Maurizio Prizzi <maurizioprizzi@gmail.com>
2022-03-08 18:34:59 -0300

    Adding files to the project

---
+++ life.py
@@ -0,0 +1,11 @@
+import math
+
+the_answer = 42
+
+# just defining a functiont that returns a text
+def quote_marvin():
+    print('I\'ve calculated your chance of survival, but I don\'t think you will like it.')
+
+# just calculate the square root of a number
+def square_root(a):
+    return math.sqrt(a)
